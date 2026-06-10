d30366bb9a04fc5fb1108aa287709e0156453c57
partho019das <partho0192710@gmail.com>
2026-06-10 20:54:20 +0600

    error

--- src/lib/auth-clint.js
+++ src/lib/auth-clint.js
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/client"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "https://urbanfashion02.vercel.app"
+    baseURL: "http://localhost:3000"
 })
