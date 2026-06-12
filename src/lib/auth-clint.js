244985a5bee0d2e3a0097beb47225d335e87a357
partho019das <partho0192710@gmail.com>
2026-06-12 12:23:50 +0600

    fix: auth config and credentials updated

--- src/lib/auth-clint.js
+++ src/lib/auth-clint.js
@@ -5,9 +5,10 @@
 // })
 
 // lib/auth-client.js
-import { createAuthClient } from "better-auth/react"
+import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-    
-    baseURL: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000"
-})
+    // এটি লোকালহোস্টে থাকলে http://localhost:3000 নিবে, 
+    // আর নেটলিফাইতে থাকলে অটোমেটিক লাইভ সাইটের লিংকটি নিয়ে নিবে!
+    baseURL: process.env.NEXT_PUBLIC_APP_URL
+});
